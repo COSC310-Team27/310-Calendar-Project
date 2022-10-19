aba6a853308fbfc2944570d6f9c4eb7cd757c218
Samantha H <hodge1214@gmail.com>
2022-10-19 14:49:37 -0400

    created object variables

--- src/Event.java
+++ src/Event.java
@@ -1,3 +1,9 @@
 public class Event {
-
+    private String name, description, location;
+    private boolean fixed;
+    private String endDate; //format given of xxxx/xx/xx which we can parse using '/' as the seperating character
+    private int priority, length;
+    private String[] timeScheduled; //format from above extended with extra /xx which will go from 1-48 with each integer marking a half hour
+    private boolean repeat;
+    private String[] repeats; //format same as timeScheduled
 }
