2383de530aad44c57beebc8f674e9eb75a4b2e42
Samantha H <hodge1214@gmail.com>
2022-10-19 14:51:41 -0400

    generated getters and setters for each variable

--- src/Event.java
+++ src/Event.java
@@ -6,4 +6,85 @@ public class Event {
     private String[] timeScheduled; //format from above extended with extra /xx which will go from 1-48 with each integer marking a half hour
     private boolean repeat;
     private String[] repeats; //format same as timeScheduled
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public void setFixed(boolean fixed) {
+        this.fixed = fixed;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String[] getTimeScheduled() {
+        return timeScheduled;
+    }
+
+    public void setTimeScheduled(String[] timeScheduled) {
+        this.timeScheduled = timeScheduled;
+    }
+
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+
+    public String[] getRepeats() {
+        return repeats;
+    }
+
+    public void setRepeats(String[] repeats) {
+        this.repeats = repeats;
+    }
 }
