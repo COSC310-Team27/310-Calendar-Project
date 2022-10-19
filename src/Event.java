aff80f6e9a2fc62d5db79d2166e5a413e12fec5e
Samantha H <hodge1214@gmail.com>
2022-10-19 15:08:44 -0400

    created default constructor created method to get current date and time in a useable format

--- src/Event.java
+++ src/Event.java
@@ -1,12 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Event {
     private String name, description, location;
     private boolean fixed;
-    private String endDate; //format given of xxxx/xx/xx which we can parse using '/' as the seperating character
+    private String endDate; //format given of xxxx-xx-xx which we can parse using '-' as the seperating character
     private int priority, length;
-    private String[] timeScheduled; //format from above extended with extra /xx which will go from 1-48 with each integer marking a half hour
+    private String[] timeScheduled; //format from above extended with extra -xx which will go from 1-48 with each integer marking a half hour
     private boolean repeat;
     private String[] repeats; //format same as timeScheduled
+    private static int num = 1;
 
+    Event(){
+        setName("Event " + num);
+        num++;
+        setFixed(false);
+        setEndDate(getCurrentDate());
+//        setPriority(); needs priority function in main scheduler
+        setLength(2);
+//        setTimeScheduled(); needs scheduling function in main scheduler
+    }
+
+    public String getCurrentDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
+    }
 
     public String getName() {
         return name;
@@ -44,8 +63,8 @@ public class Event {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
+    public void setEndDate(String currentDate) {
+        endDate = currentDate.substring(0, 8) + ((Integer.parseInt(currentDate.substring(8, 10))) + 1);
     }
 
     public int getPriority() {
